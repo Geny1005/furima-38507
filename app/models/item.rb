21df226feb8880e8f681c3c2abff257dd0417c3e
Geny1005 <progenoeyeat@gmail.com>
2022-10-11 15:32:40 +0900

    new,createルーティング設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,16 @@ class Item < ApplicationRecord
   has_one :history
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :information, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_burden_id, presence: true
+  validates :sender_area_id, presence: true
+  validates :schedule_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+
   
 
 end
