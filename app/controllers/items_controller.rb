e0b9058c1bf2070aa571a9d897a4cd4fd467cc3e
Geny1005 <progenoeyeat@gmail.com>
2022-10-12 11:14:25 +0900

    createアクションに保存されたときはルートパスに戻るよに記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
   end
 
   def new
+    @items = Item.new
   end
 
   def create
+    @items = Items.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).parmit(:name, :information, :categry_id, :condition_id, :delivery_burden_id, :sender_area_id, :schedule_id, :price).merge(user_id: current_user.id)
+    
+
 end
