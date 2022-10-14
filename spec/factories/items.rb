b0b662ca7ad590c68ec200738baf0e1acecef021
Geny1005 <progenoeyeat@gmail.com>
2022-10-14 19:28:04 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :item do
     
+    name {'商品名'}
+    information {'商品情報'}
+    category_id {'2'}
+    condition_id {'2'}
+    delivery_burden_id {'2'}
+    sender_area_id {'2'}
+    schedule_id {'2'}
+    price {'300'}
+
+    image {Faker::Lorem.sentence}
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    
   end
 end
