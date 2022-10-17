876ad302ae847f01ad9a9f7b20b69fdd35ae0275
Geny1005 <progenoeyeat@gmail.com>
2022-10-17 17:54:30 +0900

    ルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :users
+
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :items, only:[:index, :new, :create, :destroy, :edit, :update, :show]
 
-  root to: 'orders#index'
-  resources :orders, only:[:create]
 end
     
