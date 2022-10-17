0ad5021f665e1a6ea9165d95463128ca98464190
Geny1005 <progenoeyeat@gmail.com>
2022-10-17 17:47:06 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+
+  root to: 'orders#index'
+  resources :orders, only:[:create]
 end
     
