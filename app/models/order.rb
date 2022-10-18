1600b0c092c570daf249547d4f28c30b94a7dfa9
Geny1005 <progenoeyeat@gmail.com>
2022-10-18 10:08:20 +0900

    バリデーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,14 @@
 class Order < ApplicationRecord
-  validates :price, presence: true
+
+  validates :card_number, presence: true
+  validates :card_exp_month, presence: true
+  validates :card_exp_year, presence: true
+  validates :card_cvc, presence: true
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :addresses, presence: true
+  validates :building
+  validates :phone_number, presence: true
+
 end
