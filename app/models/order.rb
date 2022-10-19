6388bb7e167626fbcbd31383d041d8d4ea757dd3
Geny1005 <progenoeyeat@gmail.com>
2022-10-19 10:32:17 +0900

    バリデーション削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,8 @@
 class Order < ApplicationRecord
 
-  validates :card_number, presence: true
-  validates :card_exp_month, presence: true
-  validates :card_exp_year, presence: true
-  validates :card_cvc, presence: true
-  validates :postal_code, presence: true
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :addresses, presence: true
-  validates :building
-  validates :phone_number, presence: true
+  belongs_to :user
+  has_one :address
+  belongs_to :item
+
 
 end
