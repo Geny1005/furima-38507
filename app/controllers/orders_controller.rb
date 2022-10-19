1fd99871f3b60c42dd5cf3c0381f71720a70d64b
Geny1005 <progenoeyeat@gmail.com>
2022-10-19 14:48:41 +0900

    index,createアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,25 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = Order.new
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
     else
-      render 'index'
+      render :index
     end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],) #token:params[:token])
   end
 
 end
