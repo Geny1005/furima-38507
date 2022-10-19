5c3e67e551ef93308a879caccc7d45f4a8f92535
Geny1005 <progenoeyeat@gmail.com>
2022-10-19 20:45:57 +0900

    sender_areaへ変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,7 +19,8 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],) #token:params[:token])
+    params.require(:order_address).permit(:postal_code, :sender_area_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],) #token:params[:token])
   end
+
 
 end
