c8c07897774cb867348c5ab7633c65d126728c0e
Geny1005 <progenoeyeat@gmail.com>
2022-10-20 21:33:31 +0900

    修正③

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,9 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if @item.order.present? || @item.user != current_user
       redirect_to root_path
     else
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
 
