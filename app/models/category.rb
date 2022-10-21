5a4e8cacdb8b705b783fd306a7d801544a034c51
Geny1005 <progenoeyeat@gmail.com>
2022-10-21 11:48:14 +0900

    細かな修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,13 +4,12 @@ class Category < ActiveHash::Base
     { id: 2, name: 'レディース' },
     { id: 3, name: 'メンズ' },
     { id: 4, name: 'ベビー・キッズ' },
-    { id: 5, name: 'インテリア' },
-    { id: 6, name: '住まい' },
-    { id: 7, name: '小物' },
-    { id: 8, name: '本・音楽・ゲーム' },
-    { id: 9, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 10, name: '家電・スマホ・カメラ、スポーツ・レジャー、ハンドメイド' },
-    { id: 11, name: 'その他' },
+    { id: 5, name: 'インテリア・住まい・小物' },
+    { id: 6, name: '本・音楽・ゲーム' },
+    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, name: '家電・スマホ・カメラ' },
+    { id: 9, name: 'スポーツ・レジャー、ハンドメイド' },
+    { id: 10, name: 'その他' },
   ]
 
   include ActiveHash::Associations
